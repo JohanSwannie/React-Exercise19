5b236f850e65767c5d943bd4473ca6f539e80d6c
Johan Swan <jswannieman@gmail.com>
2024-02-15 20:52:16 +1300

    Update Showroom JSX Component

--- src/pages/Showroom.jsx
+++ src/pages/Showroom.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Showroom() {
-  return <div>Showroom</div>;
+  return (
+    <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl text-center">
+      Showroom
+    </div>
+  );
 }
 
 export default Showroom;
