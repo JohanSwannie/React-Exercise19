2d200e4978a66226dd48d2bc4b6acd0248943372
Johan Swan <jswannieman@gmail.com>
2024-02-15 20:53:39 +1300

    Update Tradeins JSX Component

--- src/pages/Tradeins.jsx
+++ src/pages/Tradeins.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Tradeins() {
   return (
     <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl text-center">
-      Tradeins
+      Trade Ins
     </div>
   );
 }
