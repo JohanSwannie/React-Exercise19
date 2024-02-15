410b042a0bb7d9d24a04d86dc525230d2707857f
Johan Swan <jswannieman@gmail.com>
2024-02-15 20:52:03 +1300

    Update Home JSX Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl text-center">
+      Home
+    </div>
+  );
 }
 
 export default Home;
