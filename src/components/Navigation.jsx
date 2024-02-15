ca9d59eb5aa5f249b04fb522a0eb5de75872237f
Johan Swan <jswannieman@gmail.com>
2024-02-15 20:36:10 +1300

    Update Navigation JSX Component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,8 +6,14 @@ function Navigation() {
       <NavLink to="/" className="text-white mr-8">
         Home
       </NavLink>
-      <NavLink to="/about" className="text-white">
+      <NavLink to="/about" className="text-white mr-8">
         About
+      </NavLink>
+      <NavLink to="/showroom" className="text-white mr-8">
+        Show Room
+      </NavLink>
+      <NavLink to="/tradeins" className="text-white mr-8">
+        Trade Ins
       </NavLink>
     </div>
   );
