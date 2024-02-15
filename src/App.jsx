efe72f3eb82aa13f99fc5e7ca7e2822f6dface58
Johan Swan <jswannieman@gmail.com>
2024-02-15 20:36:01 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Showroom from "./pages/Showroom";
+import Tradeins from "./pages/Tradeins";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/showroom" element={<Showroom />} />
+        <Route path="/tradeins" element={<Tradeins />} />
       </Routes>
     </>
   );
