8200728929b80c223a34e5670a2605b897f36ab4
Johan Swan <jswannieman@gmail.com>
2024-02-15 20:53:45 +1300

    Update Showroom JSX Component

--- src/pages/Showroom.jsx
+++ src/pages/Showroom.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Showroom() {
   return (
     <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl text-center">
-      Showroom
+      Show Room
     </div>
   );
 }
