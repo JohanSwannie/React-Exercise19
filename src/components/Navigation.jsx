472ddd9d7d3cf27d6ef06713ded55948a4e6be54
Johan Swan <jswannieman@gmail.com>
2024-02-17 14:14:40 +1300

    Update Navigation JSX Component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
-    <div className="sticky h-12 bg-black p-3 text-white grid grid-cols-12 gap-1">
+    <div className="sticky h-12 bg-[#282884] p-3 text-white grid grid-cols-12 gap-1">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/showroom">Show Room</NavLink>
