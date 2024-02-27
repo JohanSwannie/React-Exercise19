85705b65d49376a5107cb49c949e26bcc9ec0eeb
Johan Swan <jswannieman@gmail.com>
2024-02-28 10:19:43 +1300

    Update About JSX Component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,50 @@ import React from "react";
 
 function About() {
   return (
-    <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl text-center">
-      About
+    <div className="grid justify-center content-center items-center text-center text-[#23286c] tracking-wider font-bold bg-[#b6b8d4] w-screen max-h-screen">
+      <p className="mt-10 mb-10 text-3xl">About Us</p>
+      <div className="mb-10 text-left p-10">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, dolorum
+          aliquid ipsum accusamus tenetur natus nam est eum animi veniam magni
+          facere necessitatibus et ipsam ullam nulla reiciendis quaerat! Animi,
+          ad qui maxime doloremque fugit enim commodi, tempore perferendis quo
+          nostrum autem, soluta obcaecati porro reprehenderit. Qui magnam alias
+          tenetur fugiat earum quis esse architecto. Explicabo hic suscipit, in
+          alias minima libero enim molestiae, eum inventore incidunt ab
+          aspernatur quidem autem, reiciendis saepe! Hic doloremque earum
+          laboriosam, tenetur vel ducimus laudantium, quidem obcaecati alias
+          adipisci neque voluptatum sint corrupti dolores nulla optio nobis
+          nesciunt, veniam eum? Soluta quos ut ipsam.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae fugit
+          possimus perferendis voluptatum animi quo atque quis id eius eaque,
+          obcaecati ullam blanditiis quibusdam expedita repellendus quas,
+          doloremque nihil porro debitis? Vel ea nihil obcaecati sapiente illum!
+          Quos fuga, iste nemo sint asperiores, vero corporis laudantium libero
+          exercitationem doloremque et at, dignissimos fugit iure cupiditate?
+          Similique iste quibusdam autem veritatis voluptas, sint modi sit
+          tempore pariatur! Quae sit error, aut quo recusandae blanditiis
+          voluptatum dolorem ea consequatur, praesentium, magnam doloremque
+          voluptatibus repellat quaerat modi impedit voluptate accusamus
+          excepturi sint labore mollitia? Repellat nesciunt consectetur
+          quisquam, repellendus iure obcaecati mollitia eligendi, ab natus
+          libero inventore quia enim beatae quasi at vel distinctio, perferendis
+          incidunt necessitatibus. Officia eius accusamus sunt ex labore
+          perferendis a atque id tenetur quam veritatis, repudiandae fugiat!
+          Aliquid tenetur officiis omnis similique placeat nihil ut ab quibusdam
+          praesentium culpa eius quisquam pariatur rem blanditiis corporis, vero
+          iure suscipit?
+        </p>
+        <br />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias porro
+          totam unde doloremque illo numquam dicta sit, repellendus minus
+          adipisci?
+        </p>
+      </div>
     </div>
   );
 }
