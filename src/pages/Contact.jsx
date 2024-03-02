208cec424e47f92d6c4c77e4197c645efdbda401
Johan Swan <jswannieman@gmail.com>
2024-03-02 15:17:21 +1300

    Update Contact JSX Component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,51 +1,47 @@
-import React from "react";
+import { useState } from "react";
 
 function Contact() {
+  const [message, SetMessage] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    SetMessage(
+      "Thanks for contacting us. We will respond to you as soon as possible"
+    );
+  };
   return (
-    <div className="grid justify-center content-center items-center text-center text-[#23286c] tracking-wider font-bold bg-[#b6b8d4] w-screen max-h-screen">
-      <p className="mt-10 mb-10 text-3xl">Contact</p>
-      <div className="mb-10 text-left p-10">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, dolorum
-          aliquid ipsum accusamus tenetur natus nam est eum animi veniam magni
-          facere necessitatibus et ipsam ullam nulla reiciendis quaerat! Animi,
-          ad qui maxime doloremque fugit enim commodi, tempore perferendis quo
-          nostrum autem, soluta obcaecati porro reprehenderit. Qui magnam alias
-          tenetur fugiat earum quis esse architecto. Explicabo hic suscipit, in
-          alias minima libero enim molestiae, eum inventore incidunt ab
-          aspernatur quidem autem, reiciendis saepe! Hic doloremque earum
-          laboriosam, tenetur vel ducimus laudantium, quidem obcaecati alias
-          adipisci neque voluptatum sint corrupti dolores nulla optio nobis
-          nesciunt, veniam eum? Soluta quos ut ipsam.
-        </p>
-        <br />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae fugit
-          possimus perferendis voluptatum animi quo atque quis id eius eaque,
-          obcaecati ullam blanditiis quibusdam expedita repellendus quas,
-          doloremque nihil porro debitis? Vel ea nihil obcaecati sapiente illum!
-          Quos fuga, iste nemo sint asperiores, vero corporis laudantium libero
-          exercitationem doloremque et at, dignissimos fugit iure cupiditate?
-          Similique iste quibusdam autem veritatis voluptas, sint modi sit
-          tempore pariatur! Quae sit error, aut quo recusandae blanditiis
-          voluptatum dolorem ea consequatur, praesentium, magnam doloremque
-          voluptatibus repellat quaerat modi impedit voluptate accusamus
-          excepturi sint labore mollitia? Repellat nesciunt consectetur
-          quisquam, repellendus iure obcaecati mollitia eligendi, ab natus
-          libero inventore quia enim beatae quasi at vel distinctio, perferendis
-          incidunt necessitatibus. Officia eius accusamus sunt ex labore
-          perferendis a atque id tenetur quam veritatis, repudiandae fugiat!
-          Aliquid tenetur officiis omnis similique placeat nihil ut ab quibusdam
-          praesentium culpa eius quisquam pariatur rem blanditiis corporis, vero
-          iure suscipit?
-        </p>
-        <br />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias porro
-          totam unde doloremque illo numquam dicta sit, repellendus minus
-          adipisci?
-        </p>
-      </div>
+    <div className="grid justify-center content-center items-center text-center text-[#23286c] font-bold bg-[#b6b8d4] w-screen max-h-screen">
+      <p className="mt-5 mb-5 text-3xl">Contact Us</p>
+      <form action="#" onSubmit={handleSubmit} className="grid w-96 m-auto">
+        <label htmlFor="fullname" className="mb-2">
+          Full Name
+        </label>
+        <input
+          type="text"
+          name="fullname"
+          id="fullname"
+          required
+          className="mb-2"
+        />
+        <label htmlFor="email" className="mb-2">
+          Email
+        </label>
+        <input type="email" name="email" id="email" required className="mb-2" />
+        <label htmlFor="message" className="mb-2">
+          Send us a message
+        </label>
+        <textarea
+          name="message"
+          id="message"
+          cols="15"
+          rows="5"
+          className="mb-2"
+        ></textarea>
+        <input
+          type="submit"
+          className="border-2 border-white w-20 p-2 m-auto bg-stone-300"
+        />
+      </form>
+      <h4 class="mt-16 text-center m-auto">{message}</h4>
     </div>
   );
 }
