61090341de175b4afffc15318de26947acc1f0a5
Johan Swan <jswannieman@gmail.com>
2024-03-02 15:55:44 +1300

    Update UserMessage JSX Component

--- src/pages/UserMessage.jsx
+++ src/pages/UserMessage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function UserMessage() {
-  return <div>UserMessage</div>;
+function UserMessage({ userMessage }) {
+  return (
+    <div>
+      <h1 className="mt-10 h-12 text-xl">{userMessage}</h1>
+    </div>
+  );
 }
 
 export default UserMessage;
