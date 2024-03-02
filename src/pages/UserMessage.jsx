3c885cdbebdc69aec66c77ffc1d6bfe533c7ec24
Johan Swan <jswannieman@gmail.com>
2024-03-02 16:37:10 +1300

    Update UserMessage JSX Component

--- src/pages/UserMessage.jsx
+++ src/pages/UserMessage.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 
+import { Navigate } from "react-router-dom";
+
 function UserMessage({ userMessage }) {
   const [showComponent, setShowComponent] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
       setShowComponent(false);
-    }, 3000);
+    }, 4000);
   }, []);
 
   return (
     <div>
       {showComponent && <h1 className="mt-10 h-12 text-xl">{userMessage}</h1>}
+      {!showComponent && <Navigate to="/" />}
     </div>
   );
 }
