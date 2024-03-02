61d3308da9fd0a39ff7b0ab53b2af900cacf8d83
Johan Swan <jswannieman@gmail.com>
2024-03-02 15:32:20 +1300

    Update Contact JSX Component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
 
 function Contact() {
-  const [message, SetMessage] = useState("");
+  const [userMessage, SetUSerMessage] = useState("");
+  const [contactComplete, setContactComplete] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [messageInput, setMessageInput] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    SetMessage(
+    SetUSerMessage(
       "Thanks for contacting us. We will respond to you as soon as possible"
     );
-    document.getElementById("fullname").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("message").value = "";
+    setContactComplete(true);
+
+    const fullname = document.getElementById("fullname");
+    const email = document.getElementById("email");
+    const imessage = document.getElementById("message");
+    setFullName(fullname.value);
+    setEmail(email.value);
+    setMessageInput(imessage.value);
+    fullname.value = "";
+    email.value = "";
+    imessage.value = "";
   };
   return (
     <div className="grid justify-center content-center items-center text-center text-[#23286c] font-bold bg-[#b6b8d4] w-screen max-h-screen">
@@ -44,7 +57,7 @@ function Contact() {
           className="border-2 border-white w-20 p-2 m-auto bg-stone-300"
         />
       </form>
-      <h4 class="mt-16 text-center m-auto">{message}</h4>
+      {}
     </div>
   );
 }
